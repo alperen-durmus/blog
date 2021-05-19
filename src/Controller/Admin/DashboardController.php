0d760437906fabdbb6828864f7a6cf30b5e5d8ee
alperen-durmus <alperen.durmus@ideasoft.com.tr>
2021-05-19 18:55:42 +0300

    Auth işlemleri yapıldı, admine girişte anonim giriş engellendi

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Blog;
+use App\Entity\BlogCategory;
+use App\Entity\Tag;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -21,12 +25,21 @@ class DashboardController extends AbstractDashboardController
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('Blog');
+            ->setTitle('Admin')
+            ->renderContentMaximized();
     }
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        return [
+            MenuItem::linktoDashboard('Dashboard', 'fa fa-home'),
+            MenuItem::linkToCrud('Blog', 'fas fa-align-center', Blog::class),
+            MenuItem::linkToCrud('Category', 'fas fa-list', BlogCategory::class),
+            MenuItem::subMenu('Tag', 'fas fa-tags')->setSubItems([
+                MenuItem::linkToCrud('List', 'fa fa-list', Tag::class),
+                MenuItem::linkToCrud('New', 'fa fa-plus', Tag::class)->setAction('new'),
+            ]),
+        ];
     }
+    
 }
